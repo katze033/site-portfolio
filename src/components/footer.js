08063a800912830af85439acca61e206908034aa
Katze033 <josh.katzenmeyer@gmail.com>
2019-11-08 11:19:38 -0600

    Added CSS burger menu for mobile. Next: the about page.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,19 @@ import footerStyles from './modules/footer.module.css'
 const Footer = () => {
     return (
         <footer className={footerStyles.footer}>
+            <div class="social-icons">
+                <a href="https://www.twitter.com/luxpris">
+                    <i class="fab fa-twitter fa-10x"></i>
+                </a>
+                <a href="https://www.github.com/katze033">
+                    <i class="fab fa-github fa-10x"></i>
+                </a>
+                <a href="#!">
+                    <i class="fab fa-linkedin fa-10x"></i>
+                </a>
+            </div>  
             <p>Josh Katzenmeyer &copy; 2020</p>
+            
         </footer>
     )
 }
