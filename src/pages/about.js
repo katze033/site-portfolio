1f931832b8bc9358dcc8110cb248f16ab53456c9
Katze033 <josh.katzenmeyer@gmail.com>
2019-11-08 11:50:33 -0600

    Added about page text, modified footer and footer height/background color. Next: set up contact page and structure for feeding in blog posts.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,24 @@ import Layout from '../components/layout'
 const AboutPage = () => {
     return (
         <Layout>
-            <p>This is the about page.</p>
+            <div class="about--main">
+                <div class="about__paragraph">
+                    <p class="about__paragraph--body">My name's Josh Katzenmeyer, and I'm a freelance web developer skilled at
+                        creating attractive and mobile responsive websites using HTML, CSS, JavaScript, and other industry
+                        languages and tools. Whether you're looking to boost your online presence through a blog or seeking a
+                        new digital storefront for your business venture, I have the skills to design a product you'll
+                        love.<br></br></p>
+
+                    <p class="about__paragraph--body">Beyond my interest in web development, I am also passionate about
+                    exploring generative audio and visuals through code. I work with p5.js, Web Audio API, and various
+                    softwares to create art that's lively, multi-layered, and reliant on unique technologies.<br></br></p>
+
+                    <p class="about__paragraph--body">In my portfolio you'll find some of the websites and apps that I've created.<br></br></p>
+                
+                </div>
+            </div>
         </Layout>
     )
 }
-
+                                
 export default AboutPage
