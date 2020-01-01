2c27dfb1454565d5e5c614137c49d998a3ce5863
Katze033 <josh.katzenmeyer@gmail.com>
2019-12-31 20:47:08 -0600

    Added divs for project pages.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,26 @@
 import React from "react"
 import Layout from '../components/layout'
 
+import projectsStyles from '../components/modules/projects.module.css'
+
 const ProjectsPage = () => {
     return (
         <Layout>
-            <p>Coming soon.</p>
+            <div className={projectsStyles.categories_container}>
+                <div className={projectsStyles.category}>
+                    <p>Web Applications</p>
+                </div>
+                <div className={projectsStyles.category}>
+                    <p>Digital Art</p>
+                </div>
+                <div className={projectsStyles.category}>
+                    <p>Music</p>
+                </div>
+                <div className={projectsStyles.category}>
+                    <p>Resume</p>
+                </div>
+
+            </div>
         </Layout>
     )
 }
