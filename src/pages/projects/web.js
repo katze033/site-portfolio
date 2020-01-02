debd5e1212c5518f7135e88ebf5ab18c5fd4dbe3
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-02 17:31:04 -0600

    Build troubleshoot. Avoided using import and moved image to static folder.

--- src/pages/projects/web.js
+++ src/pages/projects/web.js
@@ -8,7 +8,7 @@ import categoriesStyles from '../../components/modules/projects-categories.modul
 
 
 //images
-import questForCute from '../../components/images/webprojects/quest-for-cute.png'
+//import questForCute from '../../components/images/webprojects/quest-for-cute.png'
 
 
 const ProjectsWebPage = () => {
@@ -17,7 +17,7 @@ const ProjectsWebPage = () => {
             <div>
                 <Link to="/projects">Go back</Link>
                 <div className={categoriesStyles.images_container}>
-                <a href="https://zealous-engelbart-509321.netlify.com/"><img src={questForCute} alt="A site designed for Quest For Cute, an online gift shop." className={categoriesStyles.images}></img></a>
+                <a href="https://zealous-engelbart-509321.netlify.com/"><img src={`/quest-for-cute.png`} alt="A site designed for Quest For Cute, an online gift shop." className={categoriesStyles.images}></img></a>
                 </div>
                 <p>This is a static site for Quest for Cute, an Etsy gift shop stationed out of St. Paul, Minnesota. The goal was to create a more attractive alternative to the clients' Wordpress blog. Despite needing an aesthetic overhaul, it was important for the client to still have the ability to edit content easily. To accomodate this, I coded this site using GatsbyJS and Wordpress as a headless CMS.</p>
                 <div className={categoriesStyles.images_container}>
