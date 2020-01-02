31cf0c4277d41dfbc47fb746e5a1b3782f4ca51b
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-02 17:19:31 -0600

    Troubleshooting build: commented out projecct images.

--- src/pages/projects/web.js
+++ src/pages/projects/web.js
@@ -8,7 +8,7 @@ import categoriesStyles from '../../components/modules/projects-categories.modul
 
 
 //images
-import questForCute from '../../components/images/webprojects/quest-for-cute.png'
+//import questForCute from '../../components/images/webprojects/quest-for-cute.png'
 
 const ProjectsWebPage = () => {
     return (
@@ -16,7 +16,6 @@ const ProjectsWebPage = () => {
             <div>
                 <Link to="/projects">Go back</Link>
                 <div className={categoriesStyles.images_container}>
-                <a href="https://zealous-engelbart-509321.netlify.com/"><img src={questForCute} alt="A site designed for Quest For Cute, an online gift shop." className={categoriesStyles.images}></img></a>
                 </div>
                 <p>This is a static site for Quest for Cute, an Etsy gift shop stationed out of St. Paul, Minnesota. The goal was to create a more attractive alternative to the clients' Wordpress blog. Despite needing an aesthetic overhaul, it was important for the client to still have the ability to edit content easily. To accomodate this, I coded this site using GatsbyJS and Wordpress as a headless CMS.</p>
                 <div className={categoriesStyles.images_container}>
@@ -35,3 +34,6 @@ const ProjectsWebPage = () => {
 
 export default ProjectsWebPage
 
+
+//<a href="https://zealous-engelbart-509321.netlify.com/"><img src={questForCute} alt="A site designed for Quest For Cute, an online gift shop." className={categoriesStyles.images}></img></a>
+  
