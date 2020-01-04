ece58ab7889fd4f106ea4d4a38143bbea3429f6b
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-04 13:27:57 -0600

    Reorganized images in art page with graphql and gatsby-image.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,34 +3,45 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const path = require('path')
 
 module.exports = {
   plugins: [
-      {
-        resolve: 'gatsby-plugin-web-font-loader',
-        options: {
-          google: {
-            families: ['Lato', 'sans-serif']
-          }
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Lato', 'sans-serif']
         }
-      },
-      `gatsby-plugin-react-helmet`,
-      {
-        resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: process.env.CONTENTFUL_SPACE_ID,
-          // Learn about environment variables: https://gatsby.dev/env-vars
-          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        },
-      },
-    ],
-    
-    siteMetadata: {
-      title: "Josh Katzenmeyer",
-      description:
-        "Freelance web developer, artist, and musician.",
-      url: "https://www.joshkatzenmeyer.com", // No trailing slash allowed!
-      image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
-      twitterUsername: "@luxpris",
+      }
     },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:
+      {
+        name: `images`,
+        path: path.join(__dirname, `src`, `components`, `images`),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ],
+
+  siteMetadata: {
+    title: "Josh Katzenmeyer",
+    description:
+      "Freelance web developer, artist, and musician.",
+    url: "https://www.joshkatzenmeyer.com", // No trailing slash allowed!
+    image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@luxpris",
+  },
 }
--- src/pages/projects/art.js
+++ src/pages/projects/art.js
@@ -1,46 +1,112 @@
 import React from 'react';
 
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Layout from '../../components/layout';
 
 import categoriesStyles from '../../components/modules/projects-categories.module.css'
 
+
+export const squareImage = graphql`
+  fragment squareImage on File {
+    childImageSharp {
+      fluid(maxWidth: 200, maxHeight: 200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+
+
 const ProjectsArtPage = () => {
-    return (
-        <Layout>
-            <div>
-                <Link to="/projects">Go back</Link>
-                <p>All of these images are coded using p5.js, a JavaScript library for creating graphic and interactive experiences.</p>
-                <div className={categoriesStyles.artFlex_container}>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/boathouse.png`} alt=" "></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/city.png`}></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/plane.png`}></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/plate.png`}></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/rain.png`}></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/terraform.png`}></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/treat.png`}></img>
-                    </div>
-                    <div className={categoriesStyles.art_container}>
-                        <img src={`/tunnel.png`}></img>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    )
+  const data = useStaticQuery(graphql`
+  query {
+    boathouse: file(relativePath: { eq: "art/boathouse.png" }) {
+      ...squareImage
+    }
+    city: file(relativePath: { eq: "art/city.png" }) {
+      ...squareImage
+    }
+    plane: file(relativePath: { eq: "art/plane.png" }) {
+      ...squareImage
+    }
+    plate: file(relativePath: { eq: "art/plate.png" }) {
+      ...squareImage
+    }
+    rain: file(relativePath: { eq: "art/rain.png" }) {
+      ...squareImage
+    }
+    terraform: file(relativePath: { eq: "art/terraform.png" }) {
+      ...squareImage
+    }
+    treat: file(relativePath: { eq: "art/treat.png" }) {
+      ...squareImage
+    }
+    tunnel: file(relativePath: { eq: "art/tunnel.png" }) {
+      ...squareImage
+    }
+  }
+`)
+  return (
+    <Layout>
+      <div>
+        <Link to="/projects">Go back</Link>
+        <p>All of these images are coded using p5.js, a JavaScript library for creating graphic and interactive experiences.</p>
+        <div className={categoriesStyles.artFlex_container}>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.boathouse.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.city.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.plane.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.plate.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.rain.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.terraform.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.treat.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img
+              fluid={data.tunnel.childImageSharp.fluid}
+              alt="Generative art"
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default ProjectsArtPage
