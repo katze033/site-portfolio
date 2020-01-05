4a61e1ff6bcf593348c181e080147bdc037eeb24
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-05 16:04:51 -0600

    Temporarily remove link to luxuryprison.com while I work on site edits.

--- src/pages/projects/web.js
+++ src/pages/projects/web.js
@@ -52,7 +52,7 @@ const ProjectsWebPage = () => {
         <p>This is a static site for Quest for Cute, an Etsy gift shop stationed out of St. Paul, Minnesota. The goal was to create a more attractive alternative to the clients' Wordpress blog. Despite needing an aesthetic overhaul, it was important for the client to still have the ability to edit content easily. To accomodate this, I coded this site using GatsbyJS and Wordpress as a headless CMS.</p>
         <div className={categoriesStyles.webImages_container}>
         </div>
-        <p>This is a website I coded for my personal music project. The main objective was to create a site that was fast, minimalist, and attractive. This site is comprised of simple HTML and CSS.</p>
+        
         <div className={categoriesStyles.webImages_container}>
           <a href="https://katze033-etchasketch.herokuapp.com/">
             <Img
@@ -81,4 +81,5 @@ const ProjectsWebPage = () => {
 
 export default ProjectsWebPage
 
-
+// Add later
+//<p>This is a website I coded for my personal music project. The main objective was to create a site that was fast, minimalist, and attractive. This site is comprised of simple HTML and CSS.</p>
