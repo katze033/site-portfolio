4c0b8de174ff73caeade521537083bda1e1a2c18
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-08 14:36:26 -0600

    Fixed sliding div moving with scroll on index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,17 @@ const IndexPage = () => {
     return (
         <Layout>
             <div className={indexStyles.bodyContainer}>
-            <h1 className={indexStyles.body}>Hey there.</h1>
-            <h2 className={indexStyles.body}>My name is Josh, and I am <br></br>
-                <div className={indexStyles.sliding__divContainer}>
-                    <span className={indexStyles.sliding__divRole}>a web developer.</span>
-                    <span className={indexStyles.sliding__divRole}>an artist.</span>
-                    <span className={indexStyles.sliding__divRole}>a musician.</span>
-                </div>
-            </h2>
-            <p>Read more <Link to="/about">about me.</Link></p>
+                <h1 className={indexStyles.body}>Hey there.</h1>
+                <h2 className={indexStyles.body}>My name is Josh, and I am <br></br>
+                    <div className={indexStyles.sliding__divContainerContainer}>
+                        <div className={indexStyles.sliding__divContainer}>
+                            <span className={indexStyles.sliding__divRole}>a web developer.</span>
+                            <span className={indexStyles.sliding__divRole}>an artist.</span>
+                            <span className={indexStyles.sliding__divRole}>a musician.</span>
+                        </div>
+                    </div>
+                </h2>
+                <p>Read more <Link to="/about">about me.</Link></p>
             </div>
         </Layout>
     )
