b1a92f8264a853ca43bd15a1152f86c8f5fa1e15
Katze033 <josh.katzenmeyer@gmail.com>
2020-01-19 11:26:17 -0600

    Changed px size from 200 to 300 on art and containers.

--- src/pages/projects/art.js
+++ src/pages/projects/art.js
@@ -11,7 +11,7 @@ import categoriesStyles from '../../components/modules/projects-categories.modul
 export const squareImage = graphql`
   fragment squareImage on File {
     childImageSharp {
-      fluid(maxWidth: 200, maxHeight: 200) {
+      fluid(maxWidth: 300, maxHeight: 300) {
         ...GatsbyImageSharpFluid
       }
     }
