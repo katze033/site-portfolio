12fed724ff9eb83cc3924b9da7f9ab82f5b4ced6
Katze033 <josh.katzenmeyer@gmail.com>
2020-05-20 16:11:01 -0500

    Added new works, refactored HTML code structure, began adding work titles and links to prints/superrare. Next: finish links.

--- src/pages/projects/art.js
+++ src/pages/projects/art.js
@@ -11,7 +11,7 @@ import categoriesStyles from '../../components/modules/projects-categories.modul
 export const squareImage = graphql`
   fragment squareImage on File {
     childImageSharp {
-      fluid(maxWidth: 270, maxHeight: 270) {
+      fluid(maxHeight: 270) {
         ...GatsbyImageSharpFluid
       }
     }
@@ -21,6 +21,30 @@ export const squareImage = graphql`
 const ProjectsArtPage = () => {
   const data = useStaticQuery(graphql`
   query {
+    subliminal: file(relativePath: { eq: "art/subliminal_shatter_plot.png" }) {
+      ...squareImage
+    }
+    shadow: file(relativePath: { eq: "art/shadow_transit_by_moonlight.png" }) {
+      ...squareImage
+    }
+    pavlovian: file(relativePath: { eq: "art/pavlovian_fortress.png" }) {
+      ...squareImage
+    }
+    material: file(relativePath: { eq: "art/material_daydream.png" }) {
+      ...squareImage
+    }
+    infinite: file(relativePath: { eq: "art/infinite_crossroads.png" }) {
+      ...squareImage
+    }
+    liminal: file(relativePath: { eq: "art/liminal_enclosure.png" }) {
+      ...squareImage
+    }
+    travels: file(relativePath: { eq: "art/new_travels.png" }) {
+      ...squareImage
+    }
+    spectral: file(relativePath: { eq: "art/spectral_exigency.png" }) {
+      ...squareImage
+    }
     stillness: file(relativePath: { eq: "art/stillness_delays_1600.png" }) {
       ...squareImage
     }
@@ -78,109 +102,235 @@ const ProjectsArtPage = () => {
     <Layout>
       <div>
         <Link to="/projects">More projects</Link>
-        <p>All of these images are coded with JavaScript. Along with the crypto-collectible copies I sell of these works on <a href="https://www.superrare.co/luxpris">SuperRare.co</a>, I also sell physical prints of my work at various price points. If you'd like an 8x8 or 16x16 piece on archival matte paper or canvas, feel free to <Link to={"/contact"}>contact</Link> me for a quote.</p>
+        <p>All of these images are coded using JavaScript and generative processes. You can click the links to below to purchase single edition cryptocollectible copies and physical reproductions of my work.</p>
+
         <div className={categoriesStyles.artFlex_container}>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
+              fluid={data.subliminal.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Subliminal Shatter Plot"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/shadow-transit-by-moonlight/">Print</a></p>
+            <p><a href="https://superrare.co/artwork-v2/subliminal-shatter-plot-10307">NFT</a></p>
+
+          </div>
+        <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.shadow.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Shadow Transit by Moonlight"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/shadow-transit-by-moonlight/">Print</a></p>
+            <p><a href="https://superrare.co/artwork-v2/shadow-transit-by-moonlight-9930">NFT</a></p>
+
+          </div>
+        <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.pavlovian.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Pavlovian Fortress"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.spectral.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Spectral Exigency"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.material.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Material Daydream"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/material-daydream/">Print</a></p>
+            <p><a href="">NFT</a></p>
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.infinite.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Infinite Crossroads"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/infinite-crossroads/">Print</a></p>
+            <p><a href="">NFT</a></p>
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.liminal.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"Liminal Enclosure"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/liminal-enclosure/">Print</a></p>
+            <p><a href="">NFT</a></p>
+          </div>
+          <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
+              fluid={data.travels.childImageSharp.fluid}
+              alt="Generative art"
+            />
+            <p>"New Travels"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/new-travels/">Print</a></p>
+            <p><a href="">NFT</a></p>
+          </div>
+        <div className={categoriesStyles.art_container}>
+            <Img className={categoriesStyles.image}
               fluid={data.stillness.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Stillness Delays"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.nobody.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Nobody Knows"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.fantasyexits.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Fantasy Exits"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.openchannel.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Open Channel Illuminated"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.spirit.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Spirit of the Valley"</p>
+            <p><a href="https://www.inprnt.com/gallery/luxpris/spirit-of-the-valley/">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.junkyard.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Junkyard Dreams"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.alongthecoast.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Along the Coast"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.perfectday.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"A Perfect Day for Sailing"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.portersgate.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Porter's Gate"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica8.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #8"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica7.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #7"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica6.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #6"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica5.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #5"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica4.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #4"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica3.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #3"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica2.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #2"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
           <div className={categoriesStyles.art_container}>
-            <Img
+            <Img className={categoriesStyles.image}
               fluid={data.replica1.childImageSharp.fluid}
               alt="Generative art"
             />
+            <p>"Replica Container Key #1"</p>
+            <p><a href="">Print</a></p>
+            <p><a href="">NFT</a></p>
           </div>
         </div>
         <Link to="/projects">More projects</Link>
