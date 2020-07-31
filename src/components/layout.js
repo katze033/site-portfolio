66a1993c6f726e75b6c3309ac990a6ebbe2cbaa0
Katze033 <josh.katzenmeyer@gmail.com>
2020-07-31 14:24:00 -0500

    Site fade in

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import SEO from "../components/SEO"
+import Baskerville from "../styles/fonts/LibreBaskerville-Bold.ttf"
+import Amiri from "../styles/fonts/Amiri-Regular.ttf"
+import HindGunturRegular from "../styles/fonts/HindGuntur-Regular.ttf"
+
+
 
 import '../styles/styles.css'
 
 const Layout = (props) => {
     return (
-        <body>
+        <div>
             <Helmet>
                 <title>Josh Katzenmeyer</title>
                 <html lang="en" />
+                
                 <SEO />
             </Helmet>
             {props.children}
-        </body>
+        </div >
     )
 }
 
