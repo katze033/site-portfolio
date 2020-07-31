e1727f30df2f14e1d9eade4659d8a9667cbea1d5
Katze033 <josh.katzenmeyer@gmail.com>
2020-07-31 10:38:20 -0500

    Removed unused fonts, set font-display to swap, and added SEO component to layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import SEO from "../components/SEO"
 
 import '../styles/styles.css'
 
@@ -8,6 +9,7 @@ const Layout = (props) => {
         <div>
             <Helmet>
             <title>Josh Katzenmeyer</title>
+            <SEO />
             </Helmet>
             <div>
                 {props.children}
