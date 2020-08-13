148dfe45aacfb81c270b7122eb7766c798cc261b
Katze033 <josh.katzenmeyer@gmail.com>
2020-08-13 07:01:26 -0500

    Expanded body width in mobile view.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import SEO from "../components/SEO"
-import Baskerville from "../styles/fonts/LibreBaskerville-Bold.ttf"
-import Amiri from "../styles/fonts/Amiri-Regular.ttf"
-import HindGunturRegular from "../styles/fonts/HindGuntur-Regular.ttf"
-
-
 
 import '../styles/styles.css'
 
