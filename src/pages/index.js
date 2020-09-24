e22beb99a9f3f2d420479d8cf2896b9c2df043b3
Katze033 <josh.katzenmeyer@gmail.com>
2020-09-24 17:51:02 -0500

    Updated gallery, added Rarible functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
 
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 
+import smoothscroll from 'smoothscroll-polyfill'
+
+
 const IndexPage = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
   query {
   allContentfulArtwork (sort: {fields: [datePublished], order:DESC}) {
     edges {
@@ -23,53 +26,81 @@ const IndexPage = () => {
   }
 }
 `)
-    return (
-        <Layout>
-            <h1>I'm Josh Katzenmeyer.</h1>
-            
-            <div className="gallery">
-            {data.allContentfulArtwork.edges.map((edge, i) => (
-                <Link to={`/art/${data.allContentfulArtwork.edges[i].node.slug}`}>
-                    <Img
-                        fixed={data.allContentfulArtwork.edges[i].node.previewImage.fixed}
-                        key={data.allContentfulArtwork.edges[i].node.previewImage.fixed.src} 
-                        alt={data.allContentfulArtwork.edges[i].node.title} 
-                    />
-                </Link>
-            ))}
-            </div>
-            <p>But most people online know me as <a href="https://www.twitter.com/luxpris/" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">@luxpris</a>. I am a creative technologist who assembles <a href="https://www.superrare.co/luxpris" target="_blank" rel="noopener noreferrer">abstracted landscapes and
-            collages with code.</a> I work primarily with p5.js and use generative processes while designing. The result is
+
+  useEffect(() => {
+    var gallery = document.getElementById('gallery')
+    var myTimer = setInterval(scroll, 75)
+    function scroll() {
+      smoothscroll.polyfill()
+      gallery.scrollBy({ top: 0, left: 0, behavior: 'smooth' })
+    }
+    function startScroll() {
+      myTimer = setInterval(scroll, 75)
+    }
+    function stopScroll() {
+      clearInterval(myTimer)
+    }
+    gallery.addEventListener("mouseover", stopScroll);
+    gallery.addEventListener("mouseout", startScroll);
+  })
+  
+  return (
+    <Layout>
+      <h1>I'm Josh Katzenmeyer.</h1>
+      <div
+        className="gallery"
+        id="gallery"
+      >
+        {data.allContentfulArtwork.edges.map((edge, i) => (
+          <Link to={`/art/${data.allContentfulArtwork.edges[i].node.slug}`}>
+            <Img
+              fixed={data.allContentfulArtwork.edges[i].node.previewImage.fixed}
+              key={data.allContentfulArtwork.edges[i].node.previewImage.fixed.src}
+              alt={data.allContentfulArtwork.edges[i].node.title}
+            />
+          </Link>
+        ))}
+      </div>
+      <p>But most people online know me as <a href="https://www.twitter.com/luxpris/" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">@luxpris</a>. I am a creative technologist who assembles abstracted landscapes and
+            collages with code. I work primarily with <a href="https://www.p5js.org" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">p5.js</a> and use generative processes while designing. The result is
             extraterrestrial, angular, and heavily geometric.</p>
-             <p>Conceptually I'm drawn to the limitless creative and philosophical questions unearthed when collaborating with
-            machines. What does it mean for an artist to act primarily as a curator of a computer's procedural outputs? What
+      <p>Conceptually I'm drawn to the limitless creative and philosophical questions unearthed when collaborating with
+      machines. What does it mean for an artist to act primarily as a curator of a computer's procedural outputs? What
             does it mean for these outputs to be received by and disseminated through algorithmic social media channels? Where does the user end and the machine begin?</p>
-            <p>I make a living by selling <a href="https://www.superrare.co/luxpris" target="_blank" rel="noopener noreferrer">single edition digital copies (NFTs)</a> of my work. I also
+      <p>I make a living by selling <a href="https://www.superrare.co/luxpris" target="_blank" rel="noopener noreferrer">single edition digital copies (NFTs)</a> of my work. I also
             offer web development services. If you're interested in hiring me for a commission or a custom web project, <a href="mailto:joshkatzenmeyer@gmail.com" target="_blank" rel="noopener noreferrer">email</a> me your idea and I'd love to discuss it with you.</p>
-            <h2>Exhibitions</h2>
-            <ul>
-                <li><b>Art in Quarantine</b>, <i>Wreading Digits</i>, Online, 2020</li>
-                <li><b>$1M Art Show</b>, <i>SuperRare Museum</i>, Virtual Reality, 2020</li>
-                <li><b>A Dissolution of Corrupted Arrangements</b>, <i>Museum of CryptoArt</i>, Virtual Reality, 2020 (Solo)</li>
-                <li><b>Generative & AI Art Exhibit</b>, <i>SuperRare Museum</i>, Virtual Reality, 2020</li>
-                <li><b>"Audiovisual" Album Release & Gallery</b>, <i>Studio Nouveau</i>, Virtual Reality, 2020</li>
-            </ul>
-            <h2>Press</h2>
-            <ul>
-                <li><a href="https://editorial.superrare.co/2020/09/06/an-interview-with-josh-katzenmeyer-the-artist-that-walks-in-an-abstract-landscape/" target="_blank" rel="noopener noreferrer">An Interview with Josh Katzenmeyer</a></li>
-            </ul>
-            <h2>Links</h2>
-            <ul>
-                <li><a href="https://www.superrare.co/luxpris" target="_blank" rel="noopener noreferrer">Art (NFTs)</a></li>
-                <li><a href="https://www.luxuryprison.com" target="_blank" rel="noopener noreferrer">Music</a></li>
-                <li><a href="https://twitter.com/luxpris" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://www.instagram.com/luxpris/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-                <li><a href="mailto:joshkatzenmeyer@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></li>
-            </ul>
-            <SEO />
-        </Layout>
-    )
+      <h2>Exhibitions</h2>
+      <ul>
+        <li><b>Art in Quarantine</b>, <i>Wreading Digits</i>, Online, 2020</li>
+        <li><b>$1M Art Show</b>, <i>SuperRare Museum</i>, Virtual Reality, 2020</li>
+        <li><b>A Dissolution of Corrupted Arrangements</b>, <i>Museum of CryptoArt</i>, Virtual Reality, 2020 (Solo)</li>
+        <li><b>Generative & AI Art Exhibit</b>, <i>SuperRare Museum</i>, Virtual Reality, 2020</li>
+        <li><b>"Audiovisual" Album Release & Gallery</b>, <i>Studio Nouveau</i>, Virtual Reality, 2020</li>
+      </ul>
+      <h2>Press</h2>
+      <ul>
+        <li><a href="https://editorial.superrare.co/2020/09/06/an-interview-with-josh-katzenmeyer-the-artist-that-walks-in-an-abstract-landscape/" target="_blank" rel="noopener noreferrer">An Interview with Josh Katzenmeyer</a></li>
+      </ul>
+      <h2>Links</h2>
+      <ul>
+        <li><a href="https://www.superrare.co/luxpris" target="_blank" rel="noopener noreferrer">SuperRare</a></li>
+        <li><a href="https:/app.rarible.com/luxpris" target="_blank" rel="noopener noreferrer">Rarible</a></li>
+        <li><a href="https://www.luxuryprison.com" target="_blank" rel="noopener noreferrer">Music</a></li>
+        <li><a href="https://twitter.com/luxpris" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+        <li><a href="https://www.instagram.com/luxpris/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+        <li><a href="mailto:joshkatzenmeyer@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></li>
+      </ul>
+      <SEO />
+    </Layout>
+  )
+
+
 }
+
+
+
+
+//element.scrollBy({ top: 100, left: 0, behavior: 'smooth' });
 
 
 export default IndexPage
--- src/templates/art.js
+++ src/templates/art.js
@@ -3,7 +3,6 @@ import { graphql, Link } from 'gatsby'
 import Img from "gatsby-image"
 import Web3 from 'web3'
 
-
 import Layout from '../components/layout'
 
 export const query = graphql`
@@ -17,7 +16,6 @@ export const query = graphql`
     }
     platform
     platformTokenNumber
-    linkToPlatform
   }
 }`
 
@@ -38,15 +36,15 @@ render() {
                 </div>
                 <div className="artwork-details">
                     <p><b>Platform</b></p>
-                    <p>{this.props.data.contentfulArtwork.platform}</p>
+                    <p id="platform"></p>
                     <p><b>Platform Token No.</b></p>
                     <p id="platformTokenNumber"></p>
                     <p><b>Latest Price</b></p>
-                    <p id="price"></p>
-                    <p><b>Collector</b></p>
+                    <p id="last_price"></p>
+                    <p><b>Owner</b></p>
                     <p id="collector"></p>
                     <p><b>Description</b></p>
-                    <p>Awaiting termination. 1200x1500. GIF. 2.66MB.</p>
+                    <p id="description"></p>
                 </div>
             </div>
             <Link to="/">Go Home</Link>
@@ -55,30 +53,20 @@ render() {
 }
 
 componentDidMount() {
+    this.insertLoader()
     this.loadBlockchainData()
 }
 
-async loadBlockchainData(props) {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8000")
-    const abi = [{
-            "constant": true,
-            "inputs": [
-                {
-                    "name": "tokenId",
-                    "type": "uint256"
-                }
-            ],
-            "name": "ownerOf",
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        }]
+insertLoader() {
+        let loader = "<div className=\"loader\"></div>"
+        document.getElementById("platform").innerHTML = loader;
+        document.getElementById("platformTokenNumber").innerHTML = loader;
+        document.getElementById("last_price").innerHTML = loader;
+        document.getElementById("collector").innerHTML = loader;
+        document.getElementById("description").innerHTML = loader;
+}
+
+async loadBlockchainData() {
         let contractAddress
         switch (this.props.data.contentfulArtwork.platform) {
             case 'SuperRare' :
@@ -90,29 +78,22 @@ async loadBlockchainData(props) {
                 console.log("Hmm, doesn't match an address on file")
         }
 
-
-        //Verify ownership of SuperRare tokens
-        var instance = await new web3.eth.Contract(abi, contractAddress)
-
         //Loop this
         var tokenId = this.props.data.contentfulArtwork.platformTokenNumber
-        var owner = await instance.methods.ownerOf(tokenId).call()
+        
+        
+        fetch("https://api.opensea.io/api/v1/asset/"+contractAddress+"/"+tokenId+"/?format=json")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response) 
+            document.getElementById("platform").innerHTML ="<a href=" + response.external_link + ">" + response.asset_contract.name
+            document.getElementById("platformTokenNumber").innerHTML = '<a href=https://etherscan.io/token/' + contractAddress + '?a=' + response.token_id + '>' + response.token_id
+            response.last_sale === null ? document.getElementById("last_price").innerHTML = "Not Yet Sold" : document.getElementById("last_price").innerHTML = response.last_sale.total_price/1000000000000000000 + " " + (response.last_sale.payment_token.symbol + " (~$" + (Math.floor((response.last_sale.payment_token.usd_price/1)*(response.last_sale.total_price/1000000000000000000)))) + ")"
+            response.owner.user === null ? document.getElementById("collector").innerHTML = '<a href=https://etherscan.io/address/' + response.owner.address + '>' + response.owner.address : document.getElementById("collector").innerHTML = '<a href=https://etherscan.io/address/' + response.owner.address + '>' + response.owner.user.username           
+            document.getElementById("description").innerHTML = response.description
 
-        //Response
-        document.getElementById("platformTokenNumber").innerHTML = '<a href=https://etherscan.io/token/' + contractAddress + '?a=' + tokenId + '>' + tokenId
-        document.getElementById("collector").innerHTML = '<a href=https://etherscan.io/address/' + owner + '>' + owner
+        })       
     }
-
-    
-
-
-
 }
 
 export default Art
-
-
-/*
-
-
-*/
