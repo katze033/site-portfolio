2da24b35da8a985fb93f59047fabbd316396f33d
Katze033 <josh.katzenmeyer@gmail.com>
2020-09-29 09:26:14 -0500

    Edited copy in bio, modified break rules in art description to only affect fields with id collector

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,9 +54,8 @@ const IndexPage = () => {
       <p>But most people online know me as <a href="https://www.twitter.com/luxpris/" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">@luxpris</a>. I am a creative technologist who assembles abstracted landscapes and
             collages with code. I work primarily with <a href="https://www.p5js.org" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">p5.js</a> and use generative processes while designing. The result is
             extraterrestrial, angular, and heavily geometric.</p>
-      <p>Conceptually I'm drawn to the limitless creative and philosophical questions unearthed when collaborating with
-      machines. What does it mean for an artist to act primarily as a curator of a computer's pro
-      cedural outputs? What
+      <p>Conceptually I'm drawn to the questions unearthed when collaborating with
+      machines. What does it mean for an artist to act primarily as a curator of a computer's procedural outputs? What
             does it mean for these outputs to be received by and disseminated through algorithmic social media channels? Where does the user end and the machine begin?</p>
       <p>I make a living by selling <a href="https://www.superrare.co/luxpris" target="_blank" rel="noopener noreferrer">single edition digital copies (NFTs)</a> of my work. I also
             offer web development services. If you're interested in hiring me for a commission or a custom web project, <a href="mailto:joshkatzenmeyer@gmail.com" target="_blank" rel="noopener noreferrer">email</a> me your idea and I'd love to discuss it with you.</p>
