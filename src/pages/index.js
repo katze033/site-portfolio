47220a20a873c84bfbe78f434e1ec797b8f2f190
Katze033 <josh.katzenmeyer@gmail.com>
2020-10-06 18:38:08 -0500

    Fixed typo.

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,7 +74,7 @@ const IndexPage = () => {
       <h2>Writings</h2>
       <ul>
         <li><a href="https://editorial.superrare.co/2020/10/05/an-exploration-of-high-weirdness-interviewing-sarah-zucker/" target="_blank" rel="noopener noreferrer">An Exploration of High Weirdness: Interviewing Sarah Zucker</a></li>
-        <li><a href="https://editorial.superrare.co/2020/09/03/an-interview-with-k0ch-the-algorithmic-artist-lost-in-a-recursive-loop/" target="_blank" rel="noopener noreferrer">An Interview k0ch: The Algorithmic Artist Lost in a Recursive Loop</a></li>
+        <li><a href="https://editorial.superrare.co/2020/09/03/an-interview-with-k0ch-the-algorithmic-artist-lost-in-a-recursive-loop/" target="_blank" rel="noopener noreferrer">An Interview with k0ch: The Algorithmic Artist Lost in a Recursive Loop</a></li>
         
       </ul>
       <h2>Links</h2>
