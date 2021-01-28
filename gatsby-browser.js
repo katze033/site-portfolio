b4537577b9f8f93f99fb36d74c054943b58b9877
Josh Katzenmeyer <joshkatzenmeyer@gmail.com>
2021-01-28 13:12:01 -0600

    Wrap project root with SRL

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SimpleReactLightbox from 'simple-react-lightbox'
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <SimpleReactLightbox>{element}</SimpleReactLightbox>
+  );
+}
+
