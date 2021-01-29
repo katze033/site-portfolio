84877ba3de345d6cfadd96550d6377c3a7faea92
Josh Katzenmeyer <joshkatzenmeyer@gmail.com>
2021-01-28 20:53:39 -0600

    Begin animations

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import SEO from '../components/SEO'
 
+
 const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li className="navitem"><a href="">Home</a></li>
-                <li className="navitem"><a href="">Art</a></li>
-                <li className="navitem"><a href="">Contact</a></li>
+                <li className="navitem"><a href="#home">Home</a></li>
+                <li className="navitem"><a href="#art">Art</a></li>
+                <li className="navitem"><a href="#contact">Contact</a></li>
             </ul>
+            <SEO/>
         </nav>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from 'react';
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
 
@@ -10,6 +10,11 @@ import srLogo from "../assets/sr_wordmark.svg"
 import { SRLWrapper } from "simple-react-lightbox";
 
 import NavBar from "../components/navbar"
+
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
+
+
 
 const options = {
   settings: {
@@ -54,15 +59,42 @@ const IndexPage = () => {
 }
 `)
 
+  function FadeInWhenVisible({ children }) {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [controls, inView]);
+
+    return (
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        transition={{ duration: 1 }}
+        variants={{
+          visible: { opacity: 1, translateY: 0 },
+          hidden: { opacity: 0, translateY: 200 }
+        }}
+      >
+        {children}
+      </motion.div>
+    );
+  }
 
 
   return (
-    <Layout> 
+
+    <Layout>
+
       <NavBar></NavBar>
       <video autoPlay playsInline muted loop id="video">
         <source src={video} type="video/mp4"></source>
       </video>
-      
+      <div id="home"></div>
       <section id="introduction">
 
         <div>
@@ -78,31 +110,35 @@ const IndexPage = () => {
 
         </div>
       </section>
+
       <section id="index-gallery">
-        <SRLWrapper options={options}>
+        <motion.div>
+          <SRLWrapper options={options}>
+            <FadeInWhenVisible>
+              <div className="index-gallery-container" id="art">
+                {// eslint-disable-next-line
+                }{data.allContentfulArtwork.edges.map((edge, i) => {
+                  if (i < 6) {
+                    return (
+                      <figure className={`index-gallery_item-${i}`}>
+                        <Img
+                          className='index-gallery_img'
+                          fluid={edge.node.previewImage.fluid}
+                          key={edge.node.previewImage.fluid.src}
+                          alt={edge.node.title}
+                        />
+                      </figure>
+                    )
 
-          <div className="index-gallery-container">
-          {// eslint-disable-next-line
-          }{data.allContentfulArtwork.edges.map((edge, i) => {
-              if (i < 6) {
-                return (
-                  <figure className={`index-gallery_item-${i}`}>
-                    <Img
-                      className='index-gallery_img'
-                      fluid={edge.node.previewImage.fluid}
-                      key={edge.node.previewImage.fluid.src}
-                      alt={edge.node.title}
-                    />
-                  </figure>
+                  }
+                }
                 )
+                }
 
-              }
-            }
-            )
-            }
-
-          </div>
-        </SRLWrapper>
+              </div>
+            </FadeInWhenVisible>
+          </SRLWrapper>
+        </motion.div>
 
       </section>
       <section id="work">
@@ -135,22 +171,48 @@ const IndexPage = () => {
       </section>
       <section id="contact">
         <div>
-          <a href="https://www.twitter.com/luxpris" target="_blank" rel="noopener noreferrer">
-            <img src={srLogo} id="icon" alt="SuperRare"/>
-          </a>
-          <a href="https://www.twitter.com/luxpris" target="_blank" rel="noopener noreferrer">
-            <span class="fab fa-twitter" id="icon"></span>
-          </a>
-          <a href="https://www.instagram.com/luxpris" target="_blank" rel="noopener noreferrer">
-            <span class="fab fa-instagram" id="icon"></span>
-          </a>
-          <a href="https://www.github.com/katze033" target="_blank" rel="noopener noreferrer">
-            <span class="fab fa-github" id="icon"></span>
-          </a>
-
-          <a href="mailto:joshkatzenmeyer@gmail.com" target="_blank" rel="noopener noreferrer">
-            <span class="fas fa-envelope-square" id="icon"></span>
-          </a>
+          <motion.div
+        transition={{ duration: 0.25 }}
+        whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="https://www.twitter.com/luxpris" target="_blank" rel="noopener noreferrer">
+              <img src={srLogo} id="icon" alt="SuperRare" />
+            </a>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+            border="0px"
+          >
+            <a href="https://www.twitter.com/luxpris" target="_blank" rel="noopener noreferrer">
+              <span class="fab fa-twitter" id="icon"></span>
+            </a>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="https://www.instagram.com/luxpris" target="_blank" rel="noopener noreferrer">
+              <span class="fab fa-instagram" id="icon"></span>
+            </a>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="https://www.github.com/katze033" target="_blank" rel="noopener noreferrer">
+              <span class="fab fa-github" id="icon"></span>
+            </a>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="mailto:joshkatzenmeyer@gmail.com" target="_blank" rel="noopener noreferrer">
+              <span class="fas fa-envelope-square" id="icon"></span>
+            </a>
+          </motion.div>
         </div>
       </section>
 
