f1251c3ec6b392b5b34abb76906c743ac874a700
Josh Katzenmeyer <joshkatzenmeyer@gmail.com>
2021-01-29 09:42:16 -0600

    Fix links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li className="navitem"><a href="#home">Home</a></li>
-                <li className="navitem"><a href="#art">Art</a></li>
-                <li className="navitem"><a href="#contact">Contact</a></li>
+                <li className="navitem"><a href="/#home">Home</a></li>
+                <li className="navitem"><a href="/#art">Art</a></li>
+                <li className="navitem"><a href="/#contact">Contact</a></li>
             </ul>
             <SEO/>
         </nav>
