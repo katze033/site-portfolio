9602f8c0e804214b32c849202d44c71065874b5f
Josh Katzenmeyer <joshkatzenmeyer@gmail.com>
2021-02-01 18:03:41 -0600

    Change home link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li className="navitem"><a href="/#home">Home</a></li>
+                <li className="navitem"><a href="/">Home</a></li>
                 <li className="navitem"><a href="/art">Art</a></li>
                 <li className="navitem"><a href="/#contact">Contact</a></li>
             </ul>
