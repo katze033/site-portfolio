064246b984b10bf625016f27cf49cb0e78021c52
katze033 <joshkatzenmeyer@gmail.com>
2021-03-17 16:07:26 -0500

    Remove wrap around root element, optimize bg image.

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SimpleReactLightbox from 'simple-react-lightbox'
-
-export const wrapRootElement = ({ element }) => {
-  return (
-    <SimpleReactLightbox>{element}</SimpleReactLightbox>
-  );
-}
-
